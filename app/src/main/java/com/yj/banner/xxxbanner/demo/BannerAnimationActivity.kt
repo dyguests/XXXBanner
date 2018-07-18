53e822c252767377fdfd19a9b7fecc09e96448f7
yj <1073279000@qq.com>
2018-07-18 15:53:08 +0800

    xxxBanner 动画效果解决

--- app/src/main/java/com/yj/banner/xxxbanner/demo/BannerAnimationActivity.kt
+++ app/src/main/java/com/yj/banner/xxxbanner/demo/BannerAnimationActivity.kt
@@ -4,20 +4,21 @@ package com.yj.banner.xxxbanner.demo
 import android.os.Bundle
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ListView
+import android.view.ViewGroup
 import android.widget.Toast
 import com.yj.banner.xxxbanner.App
 import com.yj.banner.xxxbanner.R
-import com.yj.banner.xxxbanner.SampleAdapter
 import com.yj.banner.xxxbanner.loader.GlideImageLoader
 import com.yj.xxxbanner.Banner
 import com.yj.xxxbanner.listener.OnBannerListener
 import com.yj.xxxbanner.transformer.*
 import kotlinx.android.synthetic.main.activity_banner_animation.*
+import kotlinx.android.synthetic.main.text_item.view.*
 
-class BannerAnimationActivity : AppCompatActivity(), AdapterView.OnItemClickListener, OnBannerListener {
+class BannerAnimationActivity : AppCompatActivity(), OnBannerListener {
     private val transformers: MutableList<Class<out ViewPager.PageTransformer>> by lazy {
         mutableListOf<Class<out ViewPager.PageTransformer>>(
                 CubeOutTransformer::class.java,
@@ -44,8 +45,21 @@ class BannerAnimationActivity : AppCompatActivity(), AdapterView.OnItemClickList
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_banner_animation)
         val data = resources.getStringArray(R.array.anim)
-        list.adapter = SampleAdapter(this, data)
-        list.onItemClickListener = this
+        recycle_view.layoutManager = LinearLayoutManager(this)
+        recycle_view.adapter=object :RecyclerView.Adapter<ViewHolder>(){
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder= ViewHolder(layoutInflater.inflate( R.layout.text_item, null))
+
+            override fun getItemCount(): Int =data.size
+
+            override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+                holder.onBind(data[position])
+                holder.itemView.apply {
+                    setOnClickListener {
+                        banner.setBannerAnimation(transformers[position])
+                    }
+                }
+            }
+        }
         (banner as Banner<String>).setImages(App.images)
                 .setImageLoader(GlideImageLoader())
                 .setOnBannerListener(this)
@@ -53,11 +67,16 @@ class BannerAnimationActivity : AppCompatActivity(), AdapterView.OnItemClickList
 
     }
 
-    override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
-        banner.setBannerAnimation(transformers[position])
-    }
 
     override fun onBannerClick(position: Int) {
         Toast.makeText(applicationContext, "你点击了：$position", Toast.LENGTH_SHORT).show()
+    }
+
+}
+
+class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+     fun onBind(data:String) {
+        itemView.text.text=data
+
     }
 }
